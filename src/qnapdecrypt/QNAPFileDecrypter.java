6e3ac7ebceb44470b62b6a96a52c297370fb1e55
Mikiya <yo.tritz@free.fr>
2017-10-07 15:37:01 +0200

    Init commit to open GIT starting from version 1.1

---
+++ src/qnapdecrypt/QNAPFileDecrypter.java
@@ -0,0 +1,274 @@
+package qnapdecrypt;
+
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * Class to request decipher QNAP ciphered files.
+ */
+
+public class QNAPFileDecrypter {
+
+	private static final int AES_KEY_STRENGTH = 256;
+
+	private static final String CIPHERED_FILE_OPTION = "i";
+
+	private static boolean dirMode = false;
+
+	private static final String JAVA_7_JCE = "http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html";
+
+	private static final String JAVA_7_VERSION = "1.7";
+
+	private static final String JAVA_8_JCE = "http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html";
+
+	private static final String JAVA_8_VERSION = "1.8";
+
+	private static final String PASSWORD_OPTION = "p";
+
+	private static final String PLAIN_FILE_OPTION = "o";
+
+	private static final String PLAIN_NAME_PREFIX = "plain_";
+
+	private static final String VERBOSE_OPTION = "v";
+
+	private static boolean verboseMode = false;
+
+	/**
+	 * Main program to decipher a file ciphered in QNAP Backup utility.
+	 * 
+	 * @param password
+	 * @param pathInputFile
+	 * @param pathOutputFile
+	 */
+	public static void main(String[] args) {
+
+		final String applicationName = "QNAPFileDecrypter";
+		if (args.length < 1) {
+			displayHeader(System.out);
+			displayHelpAndExit(applicationName);
+		}
+		CommandLine commandlineOptions = usePosixParser(args);
+
+		// Verify options
+		try {
+			if (!commandlineOptions.hasOption(CIPHERED_FILE_OPTION)
+					|| !commandlineOptions.hasOption(PLAIN_FILE_OPTION)) {
+				displayHeader(System.out);
+				displayHelpAndExit(applicationName);
+			}
+		} catch (Exception e) {
+			displayHeader(System.out);
+			displayHelpAndExit(applicationName);
+		}
+
+		if (commandlineOptions.hasOption(VERBOSE_OPTION)) {
+			verboseMode = true;
+		}
+
+		if (verboseMode) {
+			// Header
+			displayHeader(System.out);
+		}
+
+		// Check required JCE
+		try {
+			if (verboseMode) {
+				System.out.println("Check JCE policy...");
+			}
+			if (Cipher.getMaxAllowedKeyLength("AES") < AES_KEY_STRENGTH) {
+				String linkJCE = "Link not found for JCE policy";
+				if (System.getProperty("java.version").startsWith(JAVA_7_VERSION)) {
+					linkJCE = JAVA_7_JCE;
+				} else if (System.getProperty("java.version").startsWith(JAVA_8_VERSION)) {
+					linkJCE = JAVA_8_JCE;
+				} else {
+					System.err.println("JAVA version not supported, install JCE policy on JRE 7 / 8.");
+					System.exit(1);
+				}
+				System.err.println(System.lineSeparator()
+						+ "Required JCE policy not installed, use this for your version : " + System.lineSeparator()
+						+ linkJCE + System.lineSeparator() + "Instructions are provided in the JCE archive.");
+				System.exit(1);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+
+		File cipherFile = new File(commandlineOptions.getOptionValue(CIPHERED_FILE_OPTION));
+		File plainFile = new File(commandlineOptions.getOptionValue(PLAIN_FILE_OPTION));
+
+		if (cipherFile.exists() && cipherFile.isDirectory()) {
+			// To be able to decipher a directory, plain parameter must be a
+			// directory too
+			if (plainFile.exists() && plainFile.isDirectory()) {
+				dirMode = true;
+				if (verboseMode) {
+					System.out.println("Entering in directory mode.");
+				}
+			} else {
+				displayHelpAndExit(applicationName);
+			}
+		}
+
+		String password;
+		if (!commandlineOptions.hasOption(PASSWORD_OPTION)) {
+			Console console = System.console();
+			if (console == null) {
+				System.out.println("Couldn't get Console instance");
+				displayHelpAndExit(applicationName);
+			}
+			char[] passwordChars = console.readPassword("Enter user password : ");
+			password = new String(passwordChars);
+			Arrays.fill(passwordChars, ' ');
+
+			if (password == null || password.isEmpty()) {
+				System.err.println("No password entered.");
+				displayHelpAndExit(applicationName);
+			}
+		} else {
+			password = commandlineOptions.getOptionValue(PASSWORD_OPTION);
+		}
+
+		QNAPFileDecrypterEngine engine = new QNAPFileDecrypterEngine(verboseMode, dirMode);
+
+		if (!dirMode) {
+			// Single file mode
+			File outputFile = plainFile;
+			if (plainFile.isDirectory()) {
+				if (cipherFile.getParentFile().equals(plainFile)) {
+					outputFile = new File(plainFile + File.separator + PLAIN_NAME_PREFIX + cipherFile.getName());
+				} else {
+					outputFile = new File(plainFile + File.separator + cipherFile.getName());
+				}
+			} else if (cipherFile.equals(plainFile)) {
+				outputFile = new File(
+						plainFile.getParent() + File.separator + PLAIN_NAME_PREFIX + cipherFile.getName());
+			}
+			engine.doDecipherFile(cipherFile, outputFile, password);
+		} else {
+			File cipherDir = cipherFile;
+			File plainDir = plainFile;
+
+			String[] cipheredListFiles = cipherDir.list();
+			for (String eachCipheredFileName : cipheredListFiles) {
+				if (verboseMode) {
+					System.out.println(System.lineSeparator() + "Trying to decipher file : " + eachCipheredFileName);
+				}
+				String eachPlainFileName = eachCipheredFileName;
+				if (cipherDir.equals(plainDir)) {
+					eachPlainFileName = PLAIN_NAME_PREFIX + eachCipheredFileName;
+				}
+				File eachCipherFile = new File(cipherDir + File.separator + eachCipheredFileName);
+				File eachPlainFile = new File(plainDir + File.separator + eachPlainFileName);
+				if (!eachCipherFile.isDirectory() && eachCipherFile.canRead()) {
+					engine.doDecipherFile(eachCipherFile, eachPlainFile, password);
+				} else {
+					if (verboseMode) {
+						System.out.println("Skiping directory : " + eachCipheredFileName);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Construct and provide Posix-compatible Options.
+	 * 
+	 * @return Options expected from command-line of Posix form.
+	 */
+	private static Options constructPosixOptions() {
+		final Options posixOptions = new Options();
+		posixOptions.addOption(
+				Option.builder(VERBOSE_OPTION).desc("Enable verbose mode").hasArg(false).required(false).build());
+		posixOptions.addOption(
+				Option.builder(PASSWORD_OPTION).desc("User defined password").hasArg(true).required(false).build());
+		posixOptions.addOption(Option.builder(CIPHERED_FILE_OPTION)
+				.desc("Input ciphered file (or directory) to decipher").hasArg(true).required(true).build());
+		posixOptions.addOption(Option.builder(PLAIN_FILE_OPTION).desc("Output plain file (or directory)").hasArg(true)
+				.required(true).build());
+
+		return posixOptions;
+	}
+
+	/**
+	 * Display application header.
+	 * 
+	 * @out OutputStream to which header should be written.
+	 */
+	private static void displayHeader(final OutputStream out) {
+		System.out.print(System.lineSeparator());
+		final String header = "[NOT-OFFICIAL file decipher for QNAP Hybrid Backup Sync files]";
+		try {
+			out.write(header.getBytes());
+		} catch (IOException ioEx) {
+			System.out.println(header);
+		}
+		System.out.println(System.lineSeparator());
+	}
+
+	/**
+	 * Display application help and exit.
+	 * 
+	 * @out applicationName
+	 */
+	private static void displayHelpAndExit(final String applicationName) {
+		System.out.println("-- HELP --");
+		printHelp(constructPosixOptions(), 80, "COMMAND HELP",
+				System.lineSeparator()
+						+ "Note : in directory mode, both input and output arguments must be directories. Decipher operation is not recursive."
+						+ System.lineSeparator() + "END OF HELP",
+				3, 5, true, System.out);
+
+		System.exit(1);
+	}
+
+	/**
+	 * Write "help" to the provided OutputStream.
+	 */
+	private static void printHelp(final Options options, final int printedRowWidth, final String header,
+			final String footer, final int spacesBeforeOption, final int spacesBeforeOptionDescription,
+			final boolean displayUsage, final OutputStream out) {
+		final String commandLineSyntax = "java -jar qnap_decrypt_XXX.jar";
+		final PrintWriter writer = new PrintWriter(out);
+		final HelpFormatter helpFormatter = new HelpFormatter();
+		helpFormatter.printHelp(writer, printedRowWidth, commandLineSyntax, header, options, spacesBeforeOption,
+				spacesBeforeOptionDescription, footer, displayUsage);
+		writer.close();
+	}
+
+	/**
+	 * Apply Apache Commons CLI PosixParser to command-line arguments.
+	 * 
+	 * @param commandLineArguments
+	 *            Command-line arguments to be processed with Posix-style
+	 *            parser.
+	 */
+	private static CommandLine usePosixParser(final String[] commandLineArguments) {
+		final CommandLineParser cmdLinePosixParser = new DefaultParser();
+		final Options posixOptions = constructPosixOptions();
+		try {
+			return cmdLinePosixParser.parse(posixOptions, commandLineArguments);
+		} catch (ParseException parseException) {
+			System.err
+					.println("Encountered exception while parsing using PosixParser:\n" + parseException.getMessage());
+		}
+		return null;
+	}
+}
---
+++ src/qnapdecrypt/QNAPFileDecrypterEngine.java
@@ -0,0 +1,605 @@
+package qnapdecrypt;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.ShortBufferException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * Engine to decipher operations on QNAP-ciphered files.
+ */
+public class QNAPFileDecrypterEngine {
+
+	/**
+	 * Internal class for encrypted header informations.
+	 * 
+	 */
+	private class EncryptHeader {
+		long size;
+		private byte[] ckey;
+		private byte[] salt;
+
+		public byte[] getCkey() {
+			return ckey;
+		}
+
+		public byte[] getSalt() {
+			return salt;
+		}
+
+		public long getSize() {
+			return size;
+		}
+
+		public void setCkey(byte[] ckey) {
+			this.ckey = ckey;
+		}
+
+		public void setSalt(byte[] salt) {
+			this.salt = salt;
+		}
+
+		public void setSize(long size) {
+			this.size = size;
+		}
+	}
+
+	private static final int AES_KEY_STRENGTH = 256;
+
+	private static final String AES_MODE = "AES/CBC/PKCS5Padding";
+
+	private static final String AES_MODE_HEADER = "AES/ECB/NoPadding";
+
+	private static final int BLOCK_SIZE = 16;
+
+	private static final String DIGEST_ALGO = "MD5";
+
+	private static final String HEADER_EMPTY_VALUE = "\\x00";
+
+	private static final String HEADER_SEPARATOR = "\\x03";
+
+	private static final String HEADER_SPLIT_VALUE = ":";
+
+	private static final int PBKDF2_ITER_COUNT = 1000;
+
+	private static final byte[] QNAP_FILE_PREFIX_BYTES = new byte[] { 95, 95, 81, 67, 83, 95, 95 };
+
+	private static final byte[] QNAP_FILE_PREFIX_V2_BYTES = new byte[] { 75, -54, -108, 114, 94, -125, 28 };
+
+	private boolean dirMode = false;
+
+	private boolean verboseMode = false;
+
+	public QNAPFileDecrypterEngine(boolean verbose, boolean dirMode) {
+		this.dirMode = dirMode;
+		this.verboseMode = verbose;
+	}
+
+	/**
+	 * Manage decipher operations on file or directory.
+	 * 
+	 * @param cipherFile
+	 *            Ciphered file or directory.
+	 * @param plainFile
+	 *            Plain file or directory.
+	 * @param password
+	 *            User defined password.
+	 */
+	public boolean doDecipherFile(File cipherFile, File plainFile, String password) {
+		try {
+			if (!cipherFile.exists()) {
+				System.err.println("The file " + cipherFile.getName() + " does not exist.");
+				return false;
+			}
+
+			int softVersion = checkCipheredFile(cipherFile);
+			if (softVersion < 0) {
+				if (dirMode) {
+					if (verboseMode) {
+						// In dir mode, do not warn about all files not
+						// "QNAP-files" if not verbose
+						System.out.println("The file " + cipherFile.getName() + " is not a QNAP-ciphered file.");
+					}
+				} else {
+					// In single mode, file must be "QNAP-file"
+					System.err.println("The file " + cipherFile.getName() + " is not a QNAP-ciphered file.");
+				}
+				return false;
+			}
+
+			if (verboseMode) {
+				System.out.println("Reading header values...");
+			}
+			if (softVersion == 1) {
+				if (verboseMode) {
+					System.out.println("Read file version 1 !");
+				}
+				String salt = searchValueInFile(cipherFile, "salt");
+				String keyCiphered = searchValueInFile(cipherFile, "key");
+				if (verboseMode) {
+					System.out.println("Key derivation from password...");
+				}
+				SecretKeySpec uniqueKey = getPBKDF2Key(password, salt);
+				if (verboseMode) {
+					System.out.println("Decipher file key...");
+				}
+				byte[] key = decipherText(Base64.decodeBase64(keyCiphered), uniqueKey);
+				// TODO : SecretKeySpec throw a DestroyFailedException as
+				// implementation of destroy, use it when a real implementation
+				// is
+				// done
+				uniqueKey = null;
+				if (verboseMode) {
+					System.out.println("Decipher file...");
+				}
+				decipherFile(key, new byte[] {}, cipherFile, plainFile, softVersion);
+				Arrays.fill(key, Byte.MAX_VALUE);
+				if (verboseMode) {
+					System.out.println("Checking checksums from origin and output files...");
+				}
+				boolean decipherSuccess = compareChecksums(cipherFile, salt, plainFile);
+				if (!decipherSuccess) {
+					System.err.println("Wrong md5 checksum after decipher !");
+				} else if (verboseMode) {
+					System.out.println("Checksums ok, decipher " + cipherFile.getName() + " successfull !");
+				}
+			} else if (softVersion == 2) {
+				if (verboseMode) {
+					System.out.println("Read file version 2 !");
+				}
+				EncryptHeader eHeader = decipherHeader(cipherFile, password);
+
+				if (verboseMode) {
+					System.out.println("Decipher file...");
+				}
+				decipherFile(eHeader.getCkey(), eHeader.getSalt(), cipherFile, plainFile, softVersion);
+				eHeader.setCkey(null);
+
+				boolean decipherSuccess = (plainFile.length() == eHeader.getSize());
+				if (!decipherSuccess) {
+					System.err.println("Wrong size after decipher !");
+				} else if (verboseMode) {
+					System.out.println("Sizes ok, decipher " + cipherFile.getName() + " successfull !");
+				}
+			}
+		} catch (GeneralSecurityException | IOException e) {
+			System.err.println("Error occured for file " + cipherFile.getName() + ", check your password.");
+			if (verboseMode) {
+				e.printStackTrace();
+			}
+			return false;
+		}
+		return true;
+	}
+
+	public void setDirMode(boolean dirMode) {
+		this.dirMode = dirMode;
+	}
+
+	/**
+	 * Check if the ciphered file is a QNAP-ciphered file.
+	 * 
+	 * @param file
+	 */
+	private int checkCipheredFile(File cipherFile) {
+		try (final FileInputStream inputStream = new FileInputStream(cipherFile)) {
+
+			// affect read buffer and check size
+			byte[] readBytes = new byte[Math.max(QNAP_FILE_PREFIX_BYTES.length, QNAP_FILE_PREFIX_V2_BYTES.length)];
+			if (cipherFile.length() < readBytes.length) {
+				return -1;
+			}
+
+			// Read in bytes to avoid encoding errors
+			inputStream.read(readBytes);
+
+			// Compare with known headers
+			int version = -1;
+			if (readBytes.length >= QNAP_FILE_PREFIX_BYTES.length) {
+				for (int index = 0; index < readBytes.length; index++) {
+					if (QNAP_FILE_PREFIX_BYTES[index] != readBytes[index]) {
+						break;
+					} else if (index == QNAP_FILE_PREFIX_BYTES.length - 1) {
+						version = 1;
+					}
+				}
+			}
+			if (version < 0 && readBytes.length >= QNAP_FILE_PREFIX_V2_BYTES.length) {
+				for (int index = 0; index < readBytes.length; index++) {
+					if (QNAP_FILE_PREFIX_V2_BYTES[index] != readBytes[index]) {
+						break;
+					} else if (index == QNAP_FILE_PREFIX_V2_BYTES.length - 1) {
+						version = 2;
+					}
+				}
+			}
+			return version;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+
+	/**
+	 * Compare checksums from both files.
+	 * 
+	 * @param cipherFile
+	 *            Cipher file to test.
+	 * @param digestKey
+	 *            Key for decipher the checksum from cipher file.
+	 * @param plainFile
+	 *            Plain file to test.
+	 * @return
+	 */
+	private boolean compareChecksums(File cipherFile, String digestKey, File plainFile) {
+		boolean checksumFail = true;
+		try {
+			String checksumOriginCiphered = searchValueInFile(cipherFile, "digest");
+			SecretKeySpec saltAsKey = new SecretKeySpec(Hex.decodeHex(digestKey.toCharArray()), "AES");
+			byte[] checksumOriginPlain = decipherText(Base64.decodeBase64(checksumOriginCiphered), saltAsKey);
+			byte[] calculatedPlainChecksum = getChecksum(plainFile);
+			for (int checksumIndex = 0; checksumIndex < checksumOriginPlain.length; checksumIndex++) {
+				if (checksumOriginPlain[checksumIndex] != calculatedPlainChecksum[checksumIndex]) {
+					checksumFail = false;
+					break;
+				}
+			}
+		} catch (IOException | GeneralSecurityException | DecoderException e) {
+			e.printStackTrace();
+		}
+		return !checksumFail;
+	}
+
+	/**
+	 * Decipher a file.
+	 * 
+	 * @param keyArray
+	 * @param salt
+	 * @param inputFile
+	 * @param outputFile
+	 * @throws GeneralSecurityException
+	 * @throws IOException
+	 */
+	private void decipherFile(byte[] keyArray, byte[] iv, final File inputFile, final File outputFile, int version)
+			throws GeneralSecurityException, IOException {
+
+		try {
+			// Create files streams
+			final FileInputStream inputStream = new FileInputStream(inputFile);
+			final FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+			if (version == 1) {
+				// Skip file header
+				int lengthToSkip = searchDataIndexInFile(inputFile);
+				inputStream.skip(lengthToSkip);
+			} else if (version == 2) {
+				// Skip file header
+				inputStream.skip(80);
+			}
+			// Create key
+			Cipher dcipher = Cipher.getInstance(AES_MODE);
+
+			// Read random initialization vector.
+			if (version == 1) {
+				iv = new byte[BLOCK_SIZE];
+				inputStream.read(iv);
+			}
+			final IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+			// Initialize decipher
+			SecretKeySpec key = new SecretKeySpec(keyArray, "AES");
+			// Configure the cipher with the key and the iv.
+			dcipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+
+			// Read data
+			int blockSize = dcipher.getBlockSize() * dcipher.getBlockSize();
+			int outputSize = dcipher.getOutputSize(blockSize);
+			byte[] inBytes = new byte[blockSize];
+			byte[] outBytes = new byte[outputSize];
+
+			int inLength = 0;
+			boolean done = false;
+			while (!done) {
+				inLength = inputStream.read(inBytes);
+				if (inLength == blockSize) {
+					try {
+						int outLength = dcipher.update(inBytes, 0, blockSize, outBytes);
+						outputStream.write(outBytes, 0, outLength);
+					} catch (ShortBufferException e) {
+						e.printStackTrace();
+					}
+				} else
+					done = true;
+			}
+
+			try {
+				if (inLength > 0)
+					outBytes = dcipher.doFinal(inBytes, 0, inLength);
+				else
+					outBytes = dcipher.doFinal();
+				outputStream.write(outBytes);
+			} catch (IllegalBlockSizeException e) {
+				e.printStackTrace();
+			} catch (BadPaddingException e) {
+				e.printStackTrace();
+			}
+
+			inputStream.close();
+			outputStream.flush();
+			outputStream.close();
+
+		} catch (final GeneralSecurityException exc) {
+			throw exc;
+		}
+	}
+
+	/**
+	 * Decipher a header text.
+	 * 
+	 * @param textToDecipher
+	 * @param key
+	 * @throws GeneralSecurityException
+	 * @throws IOException
+	 */
+	private EncryptHeader decipherHeader(final File inputFile, String keyPass)
+			throws GeneralSecurityException, IOException {
+
+		EncryptHeader eHeader = new EncryptHeader();
+
+		try {
+			// Create files streams
+			final FileInputStream inputStream = new FileInputStream(inputFile);
+
+			// Skip file header : 16
+			inputStream.read(new byte[16]);
+			// Create key
+			Cipher dcipher = Cipher.getInstance(AES_MODE_HEADER);
+
+			// Initialize decipher
+			int iter = Math.round((1 + 32 / keyPass.length()));
+			StringBuilder builder = new StringBuilder();
+			for (int index = 0; index < iter; index++) {
+				builder.append(keyPass);
+			}
+			// Extract 32 characters from derivated password
+			String passwordFinal = builder.toString().substring(0, 32);
+			SecretKeySpec key = new SecretKeySpec(passwordFinal.getBytes(), "AES");
+			// Configure the cipher with the key and the iv.
+			dcipher.init(Cipher.DECRYPT_MODE, key);
+
+			// Read data
+			byte[] inBytes = new byte[64];
+			byte[] outBytes = new byte[64];
+			inputStream.read(inBytes);
+			outBytes = dcipher.doFinal(inBytes);
+			inputStream.close();
+
+			final ByteArrayInputStream inputHeaderStream = new ByteArrayInputStream(outBytes);
+			DataInputStream packed = new DataInputStream(inputHeaderStream);
+
+			// Struct is : magic [8] + ckey[32] + salt [16] + size [8]
+			byte[] magic = new byte[8];
+			byte[] ckey = new byte[32];
+			byte[] salt = new byte[16];
+			byte[] size = new byte[8];
+
+			// Read magic but do not use it
+			packed.read(magic, 0, 8);
+
+			packed.read(ckey, 0, 32);
+			eHeader.setCkey(ckey);
+
+			packed.read(salt, 0, 16);
+			eHeader.setSalt(salt);
+
+			packed.read(size, 0, 8);
+			ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+			buffer.put(size);
+			buffer.flip();// need flip
+			eHeader.setSize(buffer.getLong());
+
+			packed.close();
+			inputHeaderStream.close();
+
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		}
+
+		return eHeader;
+	}
+
+	/**
+	 * Decipher a text.
+	 * 
+	 * @param textToDecipher
+	 * @param key
+	 * @throws GeneralSecurityException
+	 * @throws IOException
+	 */
+	private byte[] decipherText(byte[] textToDecipher, SecretKeySpec key) throws GeneralSecurityException, IOException {
+
+		try {
+			Cipher dcipher = Cipher.getInstance(AES_MODE);
+
+			// Read random initialization vector.
+			final byte[] iv = Arrays.copyOfRange(textToDecipher, 0, BLOCK_SIZE);
+			final IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+			// Configure the cipher with the key and the iv.
+			dcipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+			final byte[] textToDecipherWithoutIv = Arrays.copyOfRange(textToDecipher, BLOCK_SIZE,
+					textToDecipher.length);
+
+			final byte[] outputBytes = dcipher.doFinal(textToDecipherWithoutIv);
+			return outputBytes;
+
+		} catch (final GeneralSecurityException exc) {
+			throw exc;
+		}
+	}
+
+	/**
+	 * Calculate the file checksum.
+	 * 
+	 * @param file
+	 * @return The file checksum.
+	 * @throws NoSuchAlgorithmException
+	 */
+	private byte[] getChecksum(File file) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance(DIGEST_ALGO);
+		try (InputStream is = Files.newInputStream(Paths.get(file.getAbsolutePath()));
+				DigestInputStream dis = new DigestInputStream(is, md)) {
+			/* Read decorated stream (dis) to EOF as normal... */
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return md.digest();
+	}
+
+	/**
+	 * Get PBKDF2 from password and salt.
+	 * 
+	 * @param pwd
+	 * @param salt
+	 * @throws GeneralSecurityException
+	 * @throws IOException
+	 */
+	private SecretKeySpec getPBKDF2Key(String pwd, String salt) {
+
+		try {
+			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			byte[] saltValue = Hex.decodeHex(salt.toCharArray());
+			KeySpec spec = new PBEKeySpec(pwd.toCharArray(), saltValue, PBKDF2_ITER_COUNT, AES_KEY_STRENGTH);
+			SecretKey tmp = factory.generateSecret(spec);
+			SecretKeySpec key = new SecretKeySpec(tmp.getEncoded(), "AES");
+			return key;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		} catch (DecoderException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	/**
+	 * Search the index in file for data (after the header).
+	 * 
+	 * @param file
+	 */
+	private int searchDataIndexInFile(File file) {
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			char[] readBuffer = new char[1024];
+			br.read(readBuffer);
+
+			String readString = new String(readBuffer);
+			String[] keys = readString.split(HEADER_SEPARATOR);
+
+			// Search for delimiter index after last property (digest)
+			int index = 0;
+			int countEmpty = 0;
+
+			boolean lastPropertyFound = false;
+			for (int stringIndex = 0; stringIndex < keys.length; stringIndex++) {
+				String eachKey = keys[stringIndex];
+				if (!lastPropertyFound) {
+					index += eachKey.length() + 1;
+				}
+				String[] keyAndValue = eachKey.split(HEADER_SPLIT_VALUE);
+				if (keyAndValue.length == 2) {
+					if ("digest".equals(keyAndValue[0])) {
+						lastPropertyFound = true;
+					}
+					if (lastPropertyFound) {
+						// Count number of empty character at the
+						// beginning of data
+						String dataStart = keys[stringIndex + 1];
+						for (int dataIndex = 0; dataIndex < dataStart.length(); dataIndex++) {
+							if (dataStart.substring(dataIndex, dataIndex + 1).matches("^" + HEADER_EMPTY_VALUE)) {
+								countEmpty++;
+							} else {
+								break;
+							}
+						}
+						index += countEmpty;
+						return index;
+					}
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return -1;
+	}
+
+	/**
+	 * Search the value in file for a header key.
+	 * 
+	 * @param file
+	 * @param inputSearch
+	 */
+	private String searchValueInFile(File file, String inputSearch) {
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			char[] readBuffer = new char[1024];
+			br.read(readBuffer);
+			try {
+				String readString = new String(readBuffer);
+				String[] keys = readString.split(HEADER_SEPARATOR);
+				for (String eachKey : keys) {
+					String[] keyAndValue = eachKey.split(HEADER_SPLIT_VALUE);
+					if (keyAndValue.length == 2) {
+						if (inputSearch.equals(keyAndValue[0])) {
+							return keyAndValue[1];
+						}
+					}
+				}
+				br.read(readBuffer);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return "";
+	}
+
+}
